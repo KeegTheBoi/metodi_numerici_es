d0e2b86cc19dbd2874ace0b14f01dd14188d859c
keeg <keegancarlo.falcao@studio.unibo.it>
2024-03-27 03:58:17 +0100

    lecture 6 done first exe

--- Lecture 6/funcutils.py
+++ Lecture 6/funcutils.py
@@ -7,19 +7,22 @@ err_abs = lambda f, g: np.abs(f - g)
 
 class FunctionApprox:
 
-    def __init__(self, sym_expr, interval, real_alfas):
+    def __init__(self, sym_expr, interval, real_alfas, x0, prev_x):
         self.sym_expr = sym_expr
         self.interval = interval
         self.real_alfas = real_alfas
         self.x_sym = sym.Symbol('x')
         self.compute()
         self.def_get_extr()
+        self.derivative()
+        self.x0 = x0
+        self.prev_x = prev_x
         
     def lambda_map(self, expr):
         return lambdify(self.x_sym, expr, np)
 
     def derivative(self, multeplicity=1):
-        return self.lambda_map(sym.diff(self.sym_expr, self.x_sym, multeplicity))
+        self.derivate = lambdify(self.x_sym, sym.diff(self.sym_expr, self.x_sym, multeplicity), np)
 
     def compute(self):
         self.func = self.lambda_map(self.sym_expr)
@@ -32,5 +35,5 @@ class FunctionApprox:
 
     def plot_error(self, method):
         self.solve_radix(method)
-        plt.semilogy(np.arange(self.i), err_abs(np.array(self.vec_xk), self.real_alfas))
+        plt.semilogy(np.arange(self.i), err_abs(np.array(self.vec_xk), self.real_alfas), "o-")
     
--- Lecture 6/radixmethods.py
+++ Lecture 6/radixmethods.py
@@ -1,11 +1,15 @@
 import math
 import numpy as np
+from more_itertools import iterate
+from itertools import takewhile
+from funcutils import FunctionApprox
 
 sign = lambda x: math.copysign(1, x)
 reg_mid = lambda a, b, f: a + (b - a) / 2
 false_mid = lambda a, b, f: a - f(a) * (b-a) / (f(b) - f(a))
 secure_bound = lambda a, b, tolx, i, maxi, fxk, tolf: abs(b - a) > tolx
-over_flow_check = lambda a, b, tolx, i, maxi, fxk, tolf:  i < maxi and secure_bound(a, b, tolx, i, maxi, fxk, tolf) and abs(fxk) > tolf
+over_flow_check = lambda a, b, tolx, i, maxi, fxk, tolf:  \
+    i < maxi and secure_bound(a, b, tolx, i, maxi, fxk, tolf) and abs(fxk) > tolf
 
 
 def non_linear(f, a, b, tolx, maxit, hypo_mid, arrest_cond, tolf=None):
@@ -39,8 +43,36 @@ def non_linear(f, a, b, tolx, maxit, hypo_mid, arrest_cond, tolf=None):
     return xk, i, v_xk
 
 
+
+def linear(f, x0, tolx, tolf, nmax, delta, dx):
+    
+    def stopping_criteria(k, x):
+        return k < nmax and abs(f(x)) >= tolf and abs(delta(x, f, dx)) >= tolx * abs(x) and abs(dx(x)) > tolf
+    
+    stream = takewhile(lambda xk: stopping_criteria(*xk), enumerate(iterate(lambda xk: xk - delta(xk, f, dx), x0)))
+    arr_xk = [v for _, v in stream]
+    if arr_xk == []:
+        print("nothing has been found")
+        
+    return None if arr_xk == [] else arr_xk[-1], len(arr_xk), arr_xk
+
+
 def bisection(f, a, b, maxit, tolx, tolf=None):
     return non_linear(f, a, b, tolx, maxit, reg_mid, secure_bound)
 
 def regula_falsi(f, a, b, maxit, tolx, given_tolf):
     return non_linear(f, a, b, tolx, maxit, false_mid, over_flow_check, tolf=given_tolf)
+
+def newton(f, x0, tolx, tolf, nmax, fa: FunctionApprox):
+    dx = fa.derivate
+    return linear(f, x0, tolx, tolf, nmax, lambda x, f, dx: f(x) / dx(x), dx)
+
+def chord(f, x0, tolx, tolf, nmax, fa: FunctionApprox):
+    dx = fa.derivate
+    m = (fa.func(fa.b) - fa.func(fa.a)) / (fa.b - fa.a)
+    return linear(f, x0, tolx, tolf, nmax, lambda x, f, dx: f(x) / m, dx)
+
+def secant(f, x0, tolx, tolf, nmax, fa: FunctionApprox):
+    dx = fa.derivate
+    m = lambda x: (fa.func(x) - fa.func(fa.prev_x)) / (x - fa.prev_x)
+    return linear(f, x0, tolx, tolf, nmax, lambda x, f, dx: f(x) / m(x), dx)
