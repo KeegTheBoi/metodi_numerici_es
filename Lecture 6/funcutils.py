a21bc960ee620c74361c1f644aad905f2b9f61c0
keeg <keegancarlo.falcao@studio.unibo.it>
2024-03-31 00:27:24 +0100

    lecture 6 advanced implement basic jacobian

--- Lecture 6/funcutils.py
+++ Lecture 6/funcutils.py
@@ -2,13 +2,14 @@ import sympy as sym
 from sympy.utilities.lambdify import lambdify
 import numpy as np
 import matplotlib.pyplot as plt
+import IPython.display as notebook
 
 err_abs = lambda f, g: np.abs(f - g)
 
 class FunctionApprox:
 
-    def __init__(self, sym_expr, interval=None, real_alfas=None, x0=None, prev_x=None, multiplicity=1):
-        self.sym_expr = sym_expr
+    def __init__(self, sym_expr, interval=None, real_alfas=None, x0=None, prev_x=None, multiplicity=1, symbols=sym.symbols('x'), notation='f'):
+        self.sym_expr = sym_expr(*symbols)
         self.interval = interval
         self.real_alfas = real_alfas
         self.x_sym = sym.Symbol('x')
@@ -18,12 +19,22 @@ class FunctionApprox:
         self.derivative(multiplicity)
         self.x0 = x0
         self.prev_x = prev_x
+        self.symbols = symbols
+        self.notation = notation
         
     def lambda_map(self, expr):
         return lambdify(self.x_sym, expr, np)
 
     def derivative(self, multiplicity):
         self.derivate = lambdify(self.x_sym, sym.diff(self.sym_expr, self.x_sym, multiplicity), np)
+
+    def partial_derivative(self, x_n: str, multiplicity=1, display=False):
+        p_diff = sym.diff(self.sym_expr, sym.symbols(x_n), multiplicity)
+        
+        if display:
+            notebook.display(notebook.Latex(r"$\frac{\partial "+self.notation+f"{self.symbols}"+r"}{\partial  "+x_n+r"}= "+f"{sym.latex(p_diff)}"+r"$"))
+        self.derivate = lambdify(self.x_sym, p_diff, np)
+        return sym.latex(p_diff)
 
     def compute(self):
         self.func = self.lambda_map(self.sym_expr)
@@ -33,6 +44,9 @@ class FunctionApprox:
 
     def solve_radix(self, method):
         alfa, self.i, self.vec_xk = method
+
+    def display_func(self):
+        notebook.display(notebook.Latex(f"${self.notation}{self.symbols}={sym.latex(self.sym_expr)}$"))
 
     def plot_error(self, method):
         self.solve_radix(method)
--- Lecture 6/radixmethods.py
+++ Lecture 6/radixmethods.py
@@ -48,13 +48,11 @@ def linear(f, x0, tolx, tolf, nmax, delta, dx):
     
     def stopping_criteria(k, x):
         return k < nmax and abs(f(x)) >= tolf and abs(delta(x, f, dx)) >= tolx * abs(x) and abs(dx(x)) > tolf
-    
-    stream = takewhile(lambda xk: stopping_criteria(*xk), enumerate(iterate(lambda xk: xk - delta(xk, f, dx), x0)))
-    arr_xk = [v for _, v in stream]
-    if arr_xk == []:
-        print("nothing has been found")
         
-    return None if arr_xk == [] else arr_xk[-1], len(arr_xk), arr_xk
+    iter = iterate(lambda xk: xk - delta(xk, f, dx), x0)
+    arr_xk = list(dict(takewhile(lambda param: stopping_criteria(*param), enumerate(iter))).values()) + [next(iter)]
+
+    return arr_xk[-1], len(arr_xk), arr_xk
 
 
 def bisection(f, a, b, maxit, tolx, tolf=None):
