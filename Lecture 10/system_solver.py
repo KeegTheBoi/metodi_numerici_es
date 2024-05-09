cb2fde92ff01df3dbb6e24987db5dcbddff310a5
Keegan Carlo Falcao <keegancarlo.falcao@studio.unibo.it>
2024-05-09 15:58:57 +0200

    removed .copies and shortened

--- Lecture 10/system_solver.py
+++ Lecture 10/system_solver.py
@@ -4,12 +4,11 @@ import scipy
 
 def LU_solve(A, b):
     PT, L, U = scipy.linalg.lu(A)
-    P = PT.copy().T
-    return Usolve(U, Lsolve(L, P @ b))
+    return Usolve(U, Lsolve(L, PT.T @ b))
 
 def cholensky_solve(A, b):
     L = lin.cholesky(A)
-    return Usolve(L.T.copy(), Lsolve(L, b))
+    return Usolve(L, Lsolve(L, b))
 
 def QR_solve(A, b):
     Q, R = scipy.linalg.qr(A)
