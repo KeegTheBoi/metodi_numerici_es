71767847729b602dcf0e0adab4805ea6b13ba7f9
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2024-05-14 11:06:06 +0200

    added some exams

---
+++ Preparazione Esame/Scheletri ed utilities-20240514/displayutils.py
@@ -0,0 +1,67 @@
+import sympy as sym
+from sympy.utilities.lambdify import lambdify
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display as nb
+
+markdown = lambda str: nb.display(nb.Markdown(str))
+latex = lambda str: nb.display(nb.Latex(str))
+
+
+def display_vector(vector, notation='v', round_n=5, center=False):
+    dollar = r'$'
+    if center:
+        dollar = r'$$'
+    str_build: str = f"{dollar}"+r" \mathbf{"+ notation + r"} = \begin{bmatrix}"
+    rows = len(vector)
+    for i in range(rows):
+        str_build += str(round(vector[i], round_n))
+        str_build += "" if i == (rows - 1)  else r" \\ "
+    
+    str_build += r"\end{bmatrix}"+f"{dollar}"
+    nb.display(nb.Latex(str_build))
+
+def plot_alfa(x, f, title="", radix=None, log=False, plotter=plt):
+    plotter.plot(x, f(x), x, x * 0, 'r')
+    plotter.scatter(radix if radix is not None else 0, 0)
+    plotter.grid(True)
+
+def display_function(func_expr, varsym=sym.symbols("x"), f='f'):
+    
+    a_parent = "("
+    l_parent = ")"
+    latex(f"${f}{a_parent}"+f"{varsym}"+f"{l_parent}="+f"{sym.latex(func_expr)}"+r"$")
+
+
+def display_matrix(matrix, notation="A(X)", round_n=5, center=False):
+    dollar = r'$'
+    if center:
+        dollar = r'$$'
+        
+    str_build: str = f"{dollar} "+ notation + r"= \begin{bmatrix}"
+    rows, cols = matrix.shape
+    for i in range(rows):
+        for j in range(cols):
+            str_build += str(round(matrix[i, j], round_n))
+            str_build += "" if j == (cols - 1)  else r" & "
+        str_build += r" \\ "
+    
+    str_build += r"\end{bmatrix}"+f'{dollar}'
+    nb.display(nb.Latex(str_build))
+    nb.display(nb.Markdown('\n'))
+
+# def plot_contour(x, y, f, i, label):
+#     col=('grey', 'blue')
+#     X, Y = np.meshgrid(x, y)
+#     ct = plt.contour(X, Y, f(X, Y), levels=0, colors=col[i])
+#     plt.clabel(ct, inline=True, colors=col[i], fmt=label)
+#     plt.grid(axis='x')
+
+# def plot_3D_projection(x, y, f):
+#     X, Y = np.meshgrid(x, y)
+#     ax = plt.figure().add_subplot(111, projection='3d')
+#     Z = f(X, Y)
+#     ax.plot_surface(X, Y, Z, color='blue', alpha = 0.7,
+#                 linewidth = 0.3, edgecolor = 'black')
+#     ax.grid(color='black')
+#     return ax
